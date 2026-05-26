5019477fa3b65f916519b62d07768cac9a77ab1b
eggenot <pangeranhezron3@gmail.com>
2026-05-26 21:54:45 +0700

    Create data.js

---
+++ montelier/js/data.js
@@ -0,0 +1,16 @@
+const monsters = [
+ {
+   name:"Slime",
+   hp:20,
+   atk:5,
+   catchRate:0.4,
+   image:"assets/slime.png"
+ },
+ {
+   name:"Fire Cub",
+   hp:30,
+   atk:8,
+   catchRate:0.25,
+   image:"assets/slime.png"
+ }
+];
